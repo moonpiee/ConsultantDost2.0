f35f8becd78961395698a294544fb1a01dd05c21
ChandPie_thecod(h)er <96528075+moonpiee@users.noreply.github.com>
2024-11-12 20:34:58 +0530

    Update ConsultantDost2.0.py

--- ConsultantDost2.0.py
+++ ConsultantDost2.0.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import os
+from groq import Groq
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.messages import HumanMessage,AIMessage
